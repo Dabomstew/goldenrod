b97bf1508cb75fc75e9ed4870634da1ca53c4c4e
Dabomstew <dbs.stew@gmail.com>
2015-05-23 22:38:57 +1200

    handout unbanner

---
+++ commands/handoutunban.py
@@ -0,0 +1,19 @@
+import config
+import random
+import datetime, time
+
+def execute(parser, bot, user, args):
+    if not parser.checkPerms(bot, user, "owner"):
+        return
+    if not args:
+        return
+    userData = bot.getUserDetails(user)
+    arglist = args.split()
+    
+    otherUserTry = arglist[0].lower()
+    
+    bot.execQueryModify("UPDATE users SET handout_ban = 0 WHERE twitchname = ?", (otherUserTry))
+    bot.channelMsg("%s -> Unbanned %s from handouts." % (user, otherUserTry))
+    
+def requiredPerm():
+    return "owner"
