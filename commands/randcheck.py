afc98a9466aa35074b1ff6ab3c8f77eec854eb98
Dabomstew <dbs.stew@gmail.com>
2015-06-02 18:05:52 +1200

    random dumper

---
+++ commands/randcheck.py
@@ -0,0 +1,12 @@
+import config
+import random
+import datetime, time
+
+def execute(parser, bot, user, args):
+    if not parser.checkPerms(bot, user, "owner"):
+        return
+    
+    print repr(random.getstate())
+    
+def requiredPerm():
+    return "owner"
