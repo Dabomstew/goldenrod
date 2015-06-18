7a1408981b66a7064a5e938c5677d21f4dfb35e5
Dabomstew <dbs.stew@gmail.com>
2015-06-19 01:00:09 +1200

    handoutbuff change

--- commands/handoutbuff.py
+++ commands/handoutbuff.py
@@ -4,7 +4,7 @@ import time
 import goldenrod, config
 
 def execute(parser, bot, user, args):
-    bot.sendInfoMessage("handoutbuff", user, "Fed up by 1-point handouts? Find the magic word to include in your !handout message to get a buff to the possible handouts. The word will change periodically. Current clue: %s" % config.handoutBuffClue)
+    bot.sendInfoMessage("handoutbuff", user, "Fed up by 1-point handouts? Find the magic word to include in your !handout message to get a buff to the possible handouts. There will be a new word each week and the word will last for 48 hours regardless of whether it is found in that time or not. Current clue: %s" % config.handoutBuffClue)
         
 def requiredPerm():
     return "anyone"
